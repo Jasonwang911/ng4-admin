97b0a51a1c61c8cd3990f9fe7dd550cb361bc4f4
jasonwang911 <396689144@qq.com>
2017-11-08 16:32:08 +0800

    ng4后台管理

--- src/app/content/banner/banner-list/banner-list.component.ts
+++ src/app/content/banner/banner-list/banner-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd';
+import { HttpService } from './../../../service/http.service';
 
 @Component({
   selector: 'app-banner-list',
@@ -7,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerListComponent implements OnInit {
 
+  data = [];
+
   userAddList = {
     username: '',
     name: '',
@@ -14,9 +18,49 @@ export class BannerListComponent implements OnInit {
     userstate: ''
   }
 
-  constructor() { }
+  isVisible = false;
+  
+  showModal = (data, i) => {
+    console.log(data);
+    console.log(i)
+    this.formData = data;
+    this.isVisible = true;
+  }
+
+  handleOk = (e) => {
+    console.log('点击了确定');
+    console.log(this.demoValue);
+    this.isVisible = false;
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.isVisible = false;
+  }
+
+
+  showConfirm = (data) => {
+    this.confirmServ.confirm({
+      title  : '您是否确认要删除这项内容',
+      content: '<b>如果删除请点击确认按钮，否则点击取消</b>',
+      onOk() {
+        console.log(data);
+      },
+      onCancel() {
+      }
+    });
+  }
+
+  // 增 和 改 的数据
+  demoValue = 3;
+  formData = {}
+
+  constructor(private httpService: HttpService, private confirmServ: NzModalService) {
+    
+  }
 
   ngOnInit() {
+    this.data = this.httpService.getProductDetail();
   }
 
   _console($event) {
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -11,6 +11,13 @@ export class HttpService {
   // 模拟数据
   // baseUrl = '';
 
+  PRD_NAME = ['爽得宝', '安鑫富-稳增计划', '华夏安康优选债券A', '博时合慧货币B（004137）'];
+  PRD_TYPE_ID = ['货币基金', '理财产品', '纯债基金'];
+  CREATE_TIME: string = '2017-10-31';
+  state = ['0', '1'];
+    
+  productDetail = [];
+
   constructor(private http: HttpClient) { }
 
   // post请求的封装
@@ -18,4 +25,24 @@ export class HttpService {
     return this.http.post(this.baseUrl + url, data)
   }
 
+  getProductDetail() {
+    for(let i = 0; i < 100; i++) {
+      this.productDetail.push(new Product(i, this.PRD_NAME[Math.floor(Math.random()*5)], this.PRD_TYPE_ID[Math.floor(Math.random()*4)], this.CREATE_TIME, this.state[Math.floor(Math.random()*2)]));
+    }
+    return this.productDetail;
+  }
+
+}
+
+export class Product {
+
+  constructor(
+    public ID: number,
+    public PRD_NAME: string,
+    public PRD_TYPE_ID: string,
+    public CREATE_TIME: string,
+    public state: string
+  ) {
+
+  }
 }
